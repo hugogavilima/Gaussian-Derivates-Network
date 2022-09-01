6a5cddb81cc5d072956d29d339f30dca478699d9
AquilesBailo <hugoisraelgavilima@gmail.com>
2022-08-31 21:58:22 -0500

    Actualizamos la generacion del GT para Shangai Part A Train. Se espera poder empezar con un entrenamiento.

--- GT_functions.py
+++ GT_functions.py
@@ -3,29 +3,36 @@ from scipy.ndimage.filters import gaussian_filter
 from scipy.spatial import KDTree 
 
 
-def gaussian_filter_density(gt, pts):
-    density = np.zeros(gt.shape, dtype=np.float32)
-    gt_count = np.count_nonzero(gt)
+def gaussian_filter_density(gts):
+    density = np.zeros(gts.shape, dtype=np.float32)
+    gt_count = np.count_nonzero(gts)
     if gt_count == 0:
         return density
 
-    f = pts.astype(int)
+    
     leafsize = 2048
-    tree = KDTree(f)
-    # query kdtree
-    #distances, locations = tree.query(pts, k=4)
+    gts_points =  np.transpose(gts.nonzero())
+    tree = KDTree(gts_points, leafsize= leafsize)
 
-    for pt in f:
-        pt2d = np.zeros(gt.shape, dtype=np.float32)
-        pt2d[pt] = 1
-        distances, locations = tree.query(pt, k=4)
+    result = np.array([], dtype = np.double)
+    
+    for pt in gts_points:
+        pt2d = np.zeros(gts.shape, dtype=np.float32)
+        try:
+            pt2d[pt[0], pt[1]] = 1
+        except:
+            print(pt)
+
+        distances, locations = tree.query(pt, k=5)
         if gt_count > 1:
-            #sigma = (distances[1]+distances[2]+distances[3])*0.1
-            sigma = 10
+           sigma = (distances.mean())*0.3
+           #sigma = 1
         else:
-            sigma = np.average(np.array(gt.shape))/2./2. #case: 1 point
-        density += gaussian_filter(pt2d, sigma, mode='constant')
-    return density
+           sigma = np.average(np.array(gt.shape))/2./2. #case: 1 point
 
+        density += gaussian_filter(pt2d, sigma, mode='constant')
+        np.append(result, sigma)
+        
+    return density, result
 
 
